31200b308f5db0bd1120e9fa129ed89f3d92ace4
VidhyaGS <ganapavi@shc:\Users\VGS\Downloads\IXD\SEM_2\TYPOGRAPHY\student-web-template2022\index.htm>
2023-04-22 14:22:24 -0400

    files updated

--- Script.js
+++ Script.js
@@ -1,3 +1,4 @@
+// This is for the dynamic greeting
 let today = new Date();
 
 let hourNow = today.getHours();
@@ -20,9 +21,10 @@ else {
 }
 
 // document.write('<h3>' + greeting + '</h3>');
-
+// The following code will bring the content to the HTML tag
 document.getElementById("greeting").innerHTML = greeting;
 
+// Thing about local storage
 let res = document.getElementById("fname");
 
 var storefname = () => {
@@ -41,7 +43,7 @@ if (typeof (Storage) !== "undefined") {
 }
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "375px";
 }
 
 function closeNav() {
@@ -63,3 +65,67 @@ function Character(){
 }
 
 Character()
+
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email');
+const submit = document.getElementsByClassName('form-contact')[0];
+
+submit.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // console.log("clicked");
+  let ebody = `
+    <h1>First Name: </h1>${fname.value}&nbsp; 
+    <br>
+    <h1>Last Name: </h1>${lname.value}
+    <br>
+    <h1>Email: </h1>${email.value}
+    <br>
+    `;
+
+  Email.send({
+    SecureToken: "e27ea19e-da2e-4d81-989f-5daa73d31f81",
+    To: 'vidhya.ganapathy10@gmail.com',
+    From: " ",
+    Subject: "Testing email from" + email.value,
+    Body: "ebody"
+  }).then(
+    message => alert("CONGRATULATIONS! Response submitted.")
+  );
+
+})
+
+// Page screens
+const loadingPage = document.getElementById("loadingPage");
+const homePage = document.getElementById("homePage");
+const learnPage = document.getElementById("learnPage");
+const connectPage = document.getElementById("connectPage");
+
+// Button for navigating in the app
+const startLearn = document.getElementById("startLearn");
+const chapterLearn = document.getElementById("chapterLearn");
+const nextPage = document.getElementById("nextPage");
+
+//Set display default for page 
+loadingPage.style.display = "block";
+homePage.style.display = "none";
+learnPage.style.display = "none";
+connectPage.style.display = "none";
+
+// For loading the home page
+startLearn.onclick = function(){
+  loadingPage.style.display = "none";
+  homePage.style.display = "block";
+};
+
+// For loading the characters page
+chapterLearn.onclick = function(){
+  homePage.style.display = "none";
+  learnPage.style.display = "block";
+};
+
+// For loading the next chapter
+nextPage.onclick = function(){
+  learnPage.style.display = "none";
+  homePage.style.display = "block";
+};
