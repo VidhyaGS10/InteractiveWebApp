dd44421ee43b818277d9aa40d2dec0b30bf0723d
VidhyaGS <ganapavi@shc:\Users\VGS\Downloads\IXD\SEM_2\TYPOGRAPHY\student-web-template2022\index.htm>
2023-04-21 21:39:23 -0400

    major updates

--- Script.js
+++ Script.js
@@ -20,3 +20,19 @@ else {
 }
 
 document.write('<h3>' + greeting + '</h3>');
+
+function Character(){
+  var xhttp = new XMLHttpRequest();//using AJAX to send a request to retrieve data from moves.json
+  xhttp.onreadystatechange = function(){
+      if (this.readyState == 4 && this.status == 200){  //check the state of moves.json
+        let hiragana = JSON.parse(this.responseText)
+        for (var i = 0; i < hiragana.length; i++){
+          console.log(hiragana[i].SubCategory)
+      }
+      }
+  }
+  xhttp.open("GET", "Script_2.json", true);
+  xhttp.send();
+}
+
+Character()
